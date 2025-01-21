4e3d544c7278d24752155fffdbccd3e1fe9c776f
Hyerim <110741@daum.net>
2025-01-21 10:14:42 +0900

    [실습] 회원가입 처리 I

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,49 @@
 import express from 'express';
+import { createUser } from './lib/user.js';
 
 // Express {}
 const app = express();
+
+// 미들웨어(Middleware)
+app.use(express.urlencoded({ extended: false }));
+
+app.post('/api/signup', async (request, response) => {
+  // 클라이언트 요청 데이터 request.body
+  /* request.body = {
+    username: 'aaaa',
+    useremail: 'dfdfd@dkjfk.com',
+    userpassword: '121212'
+  } */
+
+  const { username, useremail, userpassword } = request.body;
+
+  if (!username || !useremail || !userpassword) {
+    response.status(400).send(`
+      <p style="color: red;">회원가입에 필요한 이름, 이메일, 패스워드 모두 입력이 필요합니다.</p>
+    `);
+  }
+
+  try {
+    // 새 사용자 생성  (백엔드 스토리지)
+    const newUser = await createUser({
+      name: username,
+      email: useremail,
+      password: userpassword,
+    });
+
+    if (newUser) {
+      response
+        .status(201)
+        .send(`${newUser.name}님! 회원가입이 완료되었습니다. 😊`);
+    } else {
+      response
+        .status(400)
+        .send(`${username}님은 회원 가입을 이미 하셨습니다. 😥`);
+    }
+  } catch (error) {
+    response.status(500).send(`회원가입에 문제가 발생했습니다. 😥`);
+  }
+});
 
 // 라우팅(Routing)
 app.get('/api/hello', (request, response) => {
