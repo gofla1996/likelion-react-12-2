30b0598e028f0e641b05bd6e96a8328bf77d1014
Hyerim <110741@daum.net>
2025-01-21 10:55:47 +0900

    [실습] 회원가입 처리 II

--- backend/app.js
+++ backend/app.js
@@ -1,30 +1,29 @@
+import bcrypt from 'bcryptjs';
 import express from 'express';
 import { createUser } from './lib/user.js';
 
-// Express {}
 const app = express();
-
-// 미들웨어(Middleware)
+// 미들웨어(middleware) 설정
 app.use(express.urlencoded({ extended: false }));
 
 app.post('/api/signup', async (request, response) => {
-  // 클라이언트 요청 데이터 request.body
+  // 클라이언트 요청 데이터
   /* request.body = {
-    username: 'aaaa',
-    useremail: 'dfdfd@dkjfk.com',
-    userpassword: '121212'
-  } */
+      username: '지훈',
+      useremail: 'yamoo9@naver.com',
+      userpassword: '12345'
+    } */
 
   const { username, useremail, userpassword } = request.body;
 
   if (!username || !useremail || !userpassword) {
-    response.status(400).send(`
-      <p style="color: red;">회원가입에 필요한 이름, 이메일, 패스워드 모두 입력이 필요합니다.</p>
+    return response.status(400).send(`
+      <p style="color: red">회원가입에 필요한 이름, 이메일, 패스워드 모두 입력이 필요합니다.</p>
     `);
   }
 
   try {
-    // 새 사용자 생성  (백엔드 스토리지)
+    // 새 사용자 생성 (백엔드 스토리지)
     const newUser = await createUser({
       name: username,
       email: useremail,
@@ -34,18 +33,17 @@ app.post('/api/signup', async (request, response) => {
     if (newUser) {
       response
         .status(201)
-        .send(`${newUser.name}님! 회원가입이 완료되었습니다. 😊`);
+        .send(`${newUser.name}님! 회원가입에 성공했습니다. 😊`);
     } else {
       response
         .status(400)
         .send(`${username}님은 회원 가입을 이미 하셨습니다. 😥`);
     }
   } catch (error) {
-    response.status(500).send(`회원가입에 문제가 발생했습니다. 😥`);
+    response.status(500).send('회원가입에 문제가 발생했습니다.');
   }
 });
 
-// 라우팅(Routing)
 app.get('/api/hello', (request, response) => {
   const { username, useremail } = request.query;
   if (username && useremail) {
