bc661adf5389828dada9831741253ec8fc399578
Hyerim <110741@daum.net>
2025-01-23 14:19:38 +0900

    [실습] React 컴포넌트 속성(props) 타입 지정
    - React.ComponentProps 타입 활용
    - HTML 또는 SVG 엘리먼트 타입(문자값) 지정
    - 사용자 정의 추가 속성 확장 또는 인터섹션 타입 확장

--- src/playgrounds/02-react-form.tsx
+++ src/playgrounds/02-react-form.tsx
@@ -26,17 +26,55 @@ function ReactForm() {
     }
   };
 
+  // 컴포넌트 상태 변수 (state variable : 컴포넌트 외부의 상태 관리 시스템 기억)
+  const [photos, setPhotos] = useState<File[]>([]);
+
+  // 파생된 상태 변수 (derived stater variable)
+  const photoURLs = photos.map((photo) => URL.createObjectURL(photo));
+
+  // 상태 업데이트 핸들러 (이벤트 감지되면 실행)
+  const handleUploadPhotos = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const fileList = e.target.files;
+
+    if (fileList) {
+      setPhotos(Object.values(fileList)); // [File, File, File, ...]
+    }
+  };
+
   return (
     <div className="ReactForm">
       <h2>React 폼(form)</h2>
       <form style={formStyles}>
-        {/* type=file */}
+        <div style={{ padding: 12, border: '0.5px solid rgba(0 0 0 / 30%)' }}>
+          <FormInput
+            type="file"
+            label="포토"
+            accept=".jpg, .jpeg, .png"
+            multiple
+            onChange={handleUploadPhotos}
+          />
+          {photos.length > 0
+            ? photos.map(({ name }, index) => {
+                return (
+                  <img
+                    key={name}
+                    style={{ marginBlockStart: 8 }}
+                    src={photoURLs[index]}
+                    alt={name}
+                    width={68}
+                    height={68}
+                  />
+                );
+              })
+            : null}
+        </div>
+
+        {/* type=file (1) */}
         <div style={{ padding: 12, border: '0.5px solid rgba(0 0 0 / 30%)' }}>
           <FormInput
             type="file"
             label="프로필"
             accept="image/*"
-            // multiple
             onChange={handleUploadProfile}
           />
           {profileImage && (
@@ -154,7 +192,18 @@ function ReactForm() {
         <FormInput type="datetime-local" label="비행기 출국 시간" />
 
         <button type="submit">제출</button>
-        <button type="reset">초기화</button>
+        <button
+          type="reset"
+          onClick={() => {
+            setAge(22);
+            setColor('#2483DB');
+            setLimitAge(40);
+            setProfileImage(null);
+            setPhotos([]);
+          }}
+        >
+          초기화
+        </button>
         {/* <input type="reset" value="초기화" /> */}
       </form>
     </div>
