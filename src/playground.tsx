98b45bb746efb97d7aad0509d6437c7bffc4fa70
Hyerim <110741@daum.net>
2025-02-03 17:29:26 +0900

    실습] CVA 스타일링 - 컴포넌트 변형 정의 - 컴포넌트 변형 유지 관리

--- src/playground.tsx
+++ src/playground.tsx
@@ -1,9 +1,9 @@
 import { useState } from 'react';
 
 import Nav from '@/homework/components/nav';
+import { getUIView, type UIView } from '@/homework/lib/ui-view';
 import HomeworkSignInForm from '@/homework/pages/sign-in';
 import HomeworkSignUpForm from '@/homework/pages/sign-up';
-import { getUIView, type UIView } from '@/homework/lib/ui-view';
 
 function Playground() {
   const [uiView] = useState<UIView>(getUIView);
@@ -13,11 +13,11 @@ function Playground() {
   const isSignInView = uiView === 'signin';
 
   return (
-    <div className="Playground bg-euid-gray-200 wrapper">
+    <section className="Playground bg-euid-gray-200 wrapper">
       <h1>플레이그라운드</h1>
       <Nav />
       {isSignInView ? <HomeworkSignInForm /> : <HomeworkSignUpForm />}
-    </div>
+    </section>
   );
 }
 
