cebfeebc08ea23b408e0e92b85127fed644ada40
Hyerim <110741@daum.net>
2025-02-06 18:03:31 +0900

    [실습] Error Boundary 해설 1

---
+++ src/homework/pages/error-boundary/index.tsx
@@ -0,0 +1,14 @@
+function ErrorBoundaryDemo() {
+  return (
+    <section>
+      <h2>오류 경계</h2>
+      <p>
+        오류 경계는 자식 컴포넌트 트리의 어디에서든 JavaScript 오류를 catch하고
+        오류가 발생한 컴포넌트 트리 대신 대체 UI를 렌더링하는 React 구성
+        요소입니다.
+      </p>
+    </section>
+  );
+}
+
+export default ErrorBoundaryDemo;
