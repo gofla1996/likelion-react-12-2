4dd3287f75672fec177e05cedbeeca9e63c63a87
Hyerim <110741@daum.net>
2025-02-06 15:20:33 +0900

    [실습] 틱택토 게임 마무리 - 사실은 버그 있음 - 추후 개선 예정

--- src/homework/pages/tic-tac-toe/constants.ts
+++ src/homework/pages/tic-tac-toe/constants.ts
@@ -2,15 +2,22 @@
 // 게임 진행을 위한 상수 및 함수
 // ----------------------------------------------
 
-/* const INITIAL_CELLS = [null, null, null, null, null, null, null, null, null]; */
-export const INITIAL_CELLS = Array(9).fill(null);
-
 export const enum PLAYER {
   ONE = '😎',
   TWO = '🤢',
 }
 
-export type Cells = (PLAYER | null)[];
+export type Cell /* BoardPlayer */ = PLAYER | null;
+
+export type Cells /* Board */ = Cell[];
+
+/* const INITIAL_CELLS = [null, null, null, null, null, null, null, null, null]; */
+export const INITIAL_CELLS: Cells = Array(9).fill(null);
+
+// 다음 플레이어 반환 함수
+export const getNextPlayer = (order: number) => {
+  return order % 2 === 0 ? PLAYER.ONE : PLAYER.TWO;
+};
 
 // 게임 승리 조건
 const WINNER_CONDITIONS = [
@@ -23,10 +30,6 @@ const WINNER_CONDITIONS = [
   [0, 4, 8],
   [2, 4, 6],
 ];
-
-// 다음 플레이어 반환 함수
-export const getNextPlayer = (order: number) =>
-  order % 2 === 0 ? PLAYER.ONE : PLAYER.TWO;
 
 // 게임 진행 검사 -> 게임 위너 반환 함수
 export type Winner = {
@@ -65,10 +68,10 @@ export const getStatusMessage = (
     statusMessage = `위너! ${winner.player}`;
   }
 
-  const isDraw = !winner && cells.every((cell) => cell);
+  const isDraw = !winner && cells.every(Boolean);
 
   if (isDraw) {
-    statusMessage = '앗 비겼네!';
+    statusMessage = '비겼습니다! 😵‍💫';
   }
 
   return statusMessage;
