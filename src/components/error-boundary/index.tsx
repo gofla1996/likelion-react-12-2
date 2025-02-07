538752ffffd90b273ed14a74513520cd04fe7f48
Hyerim <110741@daum.net>
2025-02-07 14:44:43 +0900

    [실습] 오류 경계 4 - ErrorBoundary 컴포넌트는 어느 컴포넌트 트리 위치에서도 (중복) 사용 가능 - 오류가 발생한 경우, 오류 발생 컴포넌트 위치에서 가장 가까운 ErrorBoundary 컴포넌트의 대체 UI가 화면에 표시

--- src/components/error-boundary/index.tsx
+++ src/components/error-boundary/index.tsx
@@ -1,4 +1,10 @@
 import { Component, type ErrorInfo } from 'react';
+import ErrorFallbackUI from './error-fallback-ui';
+
+interface Props {
+  FallbackComponent?: typeof ErrorFallbackUI;
+  children: React.ReactNode;
+}
 
 interface State {
   error: null | Error;
@@ -6,19 +12,47 @@ interface State {
   hasError: boolean;
 }
 
-class ErrorBoundary extends Component {
+class ErrorBoundary extends Component<Props, State> {
   state: State = {
     error: null,
     errorInfo: null,
     hasError: false,
   };
 
+  // 오류 감지하기 위한 라이프 사이클 메서드
+  // static getDerivedStateFromError(error: Error) {
+  //   // 상태 업데이트
+  //   return {
+  //     hasError: true,
+  //     error,
+  //   };
+  // }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    this.setState({
+      hasError: true,
+      errorInfo,
+      error,
+    });
+  }
+
   render() {
-    return (
-      <div role="alert" lang="en">
-        <h2>Error Boundary</h2>
-      </div>
-    );
+    // 오류가 있으면
+    // 대체(fallback) 컴포넌트를 렌더링
+    if (this.state.hasError) {
+      const { error, errorInfo } = this.state;
+      const { FallbackComponent } = this.props;
+
+      return FallbackComponent ? (
+        <FallbackComponent error={error} errorInfo={errorInfo} />
+      ) : (
+        <ErrorFallbackUI error={error} errorInfo={errorInfo} />
+      );
+    }
+
+    // 오류가 없으면
+    // 하위 컴포넌트 렌더링
+    return this.props.children;
   }
 }
 
