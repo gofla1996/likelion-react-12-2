13229e82e04ff842dec1c1bbac0003bc61dcf245
Hyerim <110741@daum.net>
2025-02-10 16:16:09 +0900

    [실습] SPA 페이지와 브라우저 히스토리(기록), 로케이션 주소창 동기화 - 다시 설명

--- src/playground.tsx
+++ src/playground.tsx
@@ -5,17 +5,20 @@ import Nav from '@/components/nav';
 import Router from '@/router';
 
 function Playground() {
+  // [반응성 데이터] 화면 전환을 위한 상태 변수 선언
   const [route, setRoute] = useState(getView);
 
   // [이펙트]
-  // 리액트의 외부 시스템과 리액트 앱의 동기화
-  // 싱글 페이지 앱의 브라우저 히스토리 탐색(navigation) 기능 구현
+  // 외부 시스템인 브라우저와 클라이언트 앱인 리액트 사이에 동기화
+  // 브라우저 주소창의 `?view=값`이 변경되면 이를 읽어옵니다.
+  // 읽어온 값을 새로운 `route` 반응성 데이터 값으로 변경합니다.
   useEffect(() => {
-    // 동일한 함수 참조 (구독한 함수와 해지하려는 함수가 동일해야 함)
+    // popstate 이벤트 window 객체의 인터페이스
+    // 브라우저 탐색 버튼을 누를 때 or 프로그래밍 방식의 네비게이션 history.back() 발생
     const handlePopState = () => {
-      // 라우트(route, 길: 전환될 페이지 뷰)
-      // 사이드 이펙트 처리
-      // 브라우저 히스토리에 상태 푸시(pushState)
+      // ?view=tic-tac-toe
+      // 'tic-tac-toe'
+      setRoute(getView());
 
       // 변경된 ?view=[????] 읽기
       const params = new URLSearchParams(location.search);
@@ -27,12 +30,12 @@ function Playground() {
       if (nextRoute) setRoute(nextRoute);
     };
 
-    // popstate 이벤트 구독
-    globalThis.addEventListener('popstate', handlePopState);
+    // 이벤트 구독
+    window.addEventListener('popstate', handlePopState);
 
-    // popstate 이벤트 구독 해지(정리)
+    // 이벤트 구독 해지(정리)
     return () => {
-      globalThis.removeEventListener('popstate', handlePopState);
+      window.removeEventListener('popstate', handlePopState);
     };
   }, [route]);
 
