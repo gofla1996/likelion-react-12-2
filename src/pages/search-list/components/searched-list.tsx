70b83ccc278063c7fe65e72ef3e2d68369134821
Hyerim <110741@daum.net>
2025-02-11 15:42:23 +0900

    [실습] 검색 리스트 6 - 검색 기능 구현 - 파생된 상태 (filteredList) 활용 - list 원본 데이터 보호

--- src/pages/search-list/components/searched-list.tsx
+++ src/pages/search-list/components/searched-list.tsx
@@ -4,10 +4,34 @@ import Card from './card';
 
 interface SearchedListProps {
   list: ColorMoodItem[];
+  query: string;
   onUpdate: (item: ColorMoodItem, isFavorited: boolean) => void;
 }
 
-function SearchedList({ list, onUpdate }: SearchedListProps) {
+function SearchedList({ list, onUpdate, query }: SearchedListProps) {
+  // 사용자가 입력한 검색어(query) 소문자화
+  const word = query.toLowerCase();
+
+  // [상태 -> 속성(읽기전용) ] list
+  // [파생된 상태] filteredList = query를 기반으로 하여 list를 순환한 후, 새 리스트를 반환
+  const filteredList = list.filter((item) => {
+    // console.log(item.tags); // []
+    // console.log(item.title); // string
+    // console.log(item.description); // string
+
+    if (
+      item.title.includes(word) ||
+      item.description.includes(word) ||
+      item.tags.includes(word)
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+
+  console.log(filteredList);
+
   return (
     <section>
       <h3 className="sr-only">검색된 리스트</h3>
--- src/pages/search-list/index.tsx
+++ src/pages/search-list/index.tsx
@@ -3,9 +3,11 @@ import SearchForm from './components/search-form';
 import SearchedList from './components/searched-list';
 import colorMoodList from './data/color-mood-list';
 import { type ColorMoodItem } from './types';
+import { tm } from '@/utils/tw-merge';
 
 function SearchListPage() {
   const [list, setList] = useState<ColorMoodItem[]>(colorMoodList);
+  const [query, setQuery] = useState('');
 
   const handleUpdateList = (item: ColorMoodItem, isFavorited: boolean) => {
     const nextList = list.map((it) => {
@@ -16,10 +18,10 @@ function SearchListPage() {
   };
 
   return (
-    <section>
+    <section className={tm('flex flex-col items-center')}>
       <h2 className="text-2xl font-medium text-react">카드 검색 리스트</h2>
-      <SearchForm />
-      <SearchedList list={list} onUpdate={handleUpdateList} />
+      <SearchForm query={query} setQuery={setQuery} />
+      <SearchedList query={query} list={list} onUpdate={handleUpdateList} />
     </section>
   );
 }
