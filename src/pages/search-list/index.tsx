6e1955c6d87701030edba49b37246fd81beb5182
Hyerim <110741@daum.net>
2025-02-11 14:47:44 +0900

    [실습] 검색 리스트 5 - Card 컴포넌트 리팩토링 - 상태를 걷어냄 (제거) - 상위 컴포넌트 상태 관리로 일관

--- src/pages/search-list/index.tsx
+++ src/pages/search-list/index.tsx
@@ -1,17 +1,25 @@
 import { useState } from 'react';
-import SearchForm from './components/form';
+import SearchForm from './components/search-form';
 import SearchedList from './components/searched-list';
 import colorMoodList from './data/color-mood-list';
-import { ColorMoodItem } from './types';
+import { type ColorMoodItem } from './types';
 
 function SearchListPage() {
-  const [list] = useState<ColorMoodItem[]>(colorMoodList);
+  const [list, setList] = useState<ColorMoodItem[]>(colorMoodList);
+
+  const handleUpdateList = (item: ColorMoodItem, isFavorited: boolean) => {
+    const nextList = list.map((it) => {
+      return it.id === item.id ? { ...it, isFavorited } : it;
+    });
+
+    setList(nextList);
+  };
 
   return (
     <section>
-      <h3 className="text-2xl font-medium text-react">카드 검색 리스트</h3>
+      <h2 className="text-2xl font-medium text-react">카드 검색 리스트</h2>
       <SearchForm />
-      <SearchedList list={list} />
+      <SearchedList list={list} onUpdate={handleUpdateList} />
     </section>
   );
 }
