a2fa0d47569dea976eb7ed27da18b2b92e572e83
Hyerim <110741@daum.net>
2025-02-12 11:19:08 +0900

    [실습] 클래스와 함수 컴포넌트의 차이점 - 클래스 컴포넌트는 render() 메서드 외부의 멤버 속성 참조 가능(기억) - 함수 컴포넌트는 몸체(body) 내부의 지역 변수 다음 번 렌더링 과정에서 초기화 (기억 못함)

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -7,11 +7,17 @@ import TicTacToe from '@/pages/tic-tac-toe';
 import SignInForm from '@/pages/sign-in';
 import SignUpForm from '@/pages/sign-up';
 import NotFound from '@/pages/not-found';
+import MemoryComponentDataPage from '@/pages/memory-component-data';
 
 // --------------------------------------------------------------------------
 // 내비게이션 리스트 데이터
 
 export const navList = [
+  {
+    path: 'memory-component-data',
+    text: '데이터 메모리',
+    element: <MemoryComponentDataPage />,
+  },
   { path: 'search-list', text: '검색 리스트', element: <SearchListPage /> },
   { path: 'side-effect', text: '이펙트 처리', element: <SideEffectDemo /> },
   {
