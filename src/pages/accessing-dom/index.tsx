e07b21bbc50dc963b48a3f333688134cd64770ff
Hyerim <110741@daum.net>
2025-02-12 16:26:12 +0900

    [실습] DOM 접근/조작 2.1 - ref 콜백 활용 - 이펙트 함수 활용

---
+++ src/pages/accessing-dom/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function AccesDOMPage() {
+  return (
+    <section>
+      <h2>
+        <abbr title="Document Object Model">DOM 접근/조작</abbr>
+      </h2>
+    </section>
+  );
+}
+
+export default AccesDOMPage;
