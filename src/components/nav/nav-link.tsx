9cfafaf1b66534a712a66f1c519701d849b1b01f
Hyerim <110741@daum.net>
2025-02-12 09:57:12 +0900

    [실습] 검색 리스트 10 - 페이지 전환 시, 검색 쿼리 삭제 (변경)

--- src/components/nav/nav-link.tsx
+++ src/components/nav/nav-link.tsx
@@ -1,13 +1,14 @@
-import { getView, setView } from '@/router/manage-view';
 import { tm } from '@/utils/tw-merge';
+import { getView, resetActiveFocus, setView } from '@/router/manage-view';
+import { deleteQueryParam } from '@/pages/search-list/utils/query-param';
 
 type WithoutHref = Omit<React.ComponentProps<'a'>, 'href'>;
 type OnChangeRoute = React.Dispatch<React.SetStateAction<string>>;
 
 type NavLinkProps = WithoutHref & {
-  href: string;
   onChangeRoute: OnChangeRoute;
   activeClassName?: string;
+  href: string;
 };
 
 function NavLink({
@@ -26,7 +27,12 @@ function NavLink({
     e.preventDefault();
     // 프로그래밍 방식 내비게이션 설정
     onChangeRoute(href);
+    // Playground 컴포넌트의 상태 업데이트 요청
     setView(href);
+    // 페이지 이동 후, 문서에 초점이동
+    resetActiveFocus();
+    // 다른 페이지 이동 시, 검색 쿼리 삭제
+    deleteQueryParam();
   };
 
   return (
