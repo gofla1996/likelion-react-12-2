e7a05a0622d616ad62b095af58d78cb827adcdc2
Hyerim <110741@daum.net>
2025-02-19 10:30:27 +0900

    [실습] 보다 향상된 사용자 경험 (UX) - useFetchData vs. useQuery - TanStack Query (React Query) 모방 - 클라이언트 측에 데이터 캐싱(caching)하면 사용자 경험 향상

--- src/pages/custom-hook/components/pokemon-with-use-fetch-data.tsx
+++ src/pages/custom-hook/components/pokemon-with-use-fetch-data.tsx
@@ -1,15 +1,11 @@
 import { tm } from '@/utils/tw-merge';
 import Loading from '@/pages/memo-list/components/loading';
 import { useFetchData } from '@/hooks/use-fetch-data';
-import type { Pokemon as PokemonSingle } from '@/types/pokemon';
+import type { Pokemon as PokemonSingle } from '../types';
 
 function Pokemon() {
   // 포켓몬 개별 정보 가져오기
   // 'https://pokeapi.co/api/v2/pokemon/1'
-
-  // const loading = true;
-  // const error = null;
-  // const data = null;
 
   const { loading, error, data } = useFetchData<PokemonSingle>(
     'https://pokeapi.co/api/v2/pokemon/25'
@@ -18,9 +14,9 @@ function Pokemon() {
   return (
     <figure
       className={tm(
-        'flex gap-5 justify-center',
+        'flex gap-5 justify-center items-center',
         'border-4 border-black/10',
-        'rounded-full p-6',
+        'rounded-full h-42',
         'transition-colors duration-250 ease-in-out',
         'hover:border-black'
       )}
@@ -28,8 +24,13 @@ function Pokemon() {
       {/* <Loading size={24} label="데이터 로딩 중..." className="text-black/50" /> */}
       {loading && <Loading size={48} label="포켓몬 데이터 로딩 중..." />}
       {error && <div role="alert">{error.message}</div>}
-      {data && (
-        <pre>{JSON.stringify((data as { cries: [] }).cries, null, 2)}</pre>
+      {!loading && data && (
+        <img
+          src={data.sprites.front_default}
+          alt={data.name}
+          title={data.name}
+          className="size-40"
+        />
       )}
     </figure>
   );
