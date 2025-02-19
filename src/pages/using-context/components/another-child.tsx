152e3bc0ebe400e968bbd5d6937992a49c74045d
Hyerim <110741@daum.net>
2025-02-19 15:21:21 +0900

    [준비] Context API 4, 5 - 컨텍스트 생성 - 컨텍스트 범위 지정 준비
    - 컨텍스트 범위 안에서만 공유 가능
    - 컨텍스트 외부에서는 초기 상태 값만 공유 가능(업데이트는 불가능)

--- src/pages/using-context/components/another-child.tsx
+++ src/pages/using-context/components/another-child.tsx
@@ -1,17 +1,19 @@
 import { useContext } from 'react';
 import { GreetingContext } from '../page';
 
-function GrandChild() {
+function AnotherChild() {
   const { message, setMessage } = useContext(GreetingContext);
 
+  console.log(message, setMessage);
+
   return (
-    <div className="flex-1 flex flex-col gap-3 p-5 border-4 rounded-full justify-center items-center text-center">
+    <div className="flex-1 flex flex-col gap-3 p-5 border-4 rounded-full items-center justify-center text-center">
       {message}
       <button
         type="button"
         className="bg-react text-white p-2 text-sm rounded-full w-50"
         onClick={() => {
-          setMessage('반갑습니다. Mr. 페이지!');
+          setMessage('반갑습니다. Mr. 페이지! 저는 AnotherChild에요!');
         }}
       >
         Page 컴포넌트에게 인사하기
@@ -20,4 +22,4 @@ function GrandChild() {
   );
 }
 
-export default GrandChild;
+export default AnotherChild;
