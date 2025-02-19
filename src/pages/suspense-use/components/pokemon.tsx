f768d8fcf333d0d7e5e112572e82fae10a9ee433
Hyerim <110741@daum.net>
2025-02-19 13:51:06 +0900

    [실습] Suspense + use() 활용 2 - 클라이언트 캐시 활용 - fetchPokemon 비동기 함수 작성

--- src/pages/suspense-use/components/pokemon.tsx
+++ src/pages/suspense-use/components/pokemon.tsx
@@ -1,10 +1,19 @@
+import { use } from 'react';
 import PokemonLayout from './pokemon-layout';
+import { fetchPokemon } from '../api/pokemon';
 
-function Pokemon() {
+interface PokemonProps {
+  id: number;
+}
+
+function Pokemon({ id }: PokemonProps) {
+  const pokemon = use(fetchPokemon(id));
+  console.log(pokemon.id);
+
   return (
     <PokemonLayout>
       <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"
+        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
         alt="pikachu"
         title="pikachu"
         className="size-28"
